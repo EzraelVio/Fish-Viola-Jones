fd8d02e701ef7ddefdf3f347aec039a23557175d
Nehemiah Austen Pison <ezraelvio@yahoo.co.id>
2023-07-31 14:54:41 +0700

    31072023 update

--- NewMethods.py
+++ NewMethods.py
@@ -3,35 +3,41 @@ import cv2
 import os
 
 class TrainingImage:
-    def make_integral_image(h, w, ColorChannel, image):
-        ChannelName = str(ColorChannel)
+    def make_integral_image(self, h, w, ColorChannel, integral_image):
         for i in range(h):
             for j in range(w):
-                f"integral_image_{ChannelName}"[i][j] = image[i][j]
+                integral_image[i][j] = ColorChannel[i][j]
         
-        f"integral_image_{ChannelName}"[0][0] = image[0][0]
+        integral_image[0][0] = ColorChannel[0][0]
         #Calculating 1st row integral image
         for i in range(h-1):
-            f"integral_image_{ChannelName}"[i+1][0] = image[i+1][0] + f"integral_image_{ChannelName}"[i][0]
+            integral_image[i+1][0] = ColorChannel[i+1][0] + integral_image[i][0]
         #Calculating 1st coloum integral image
         for i in range(w-1):
-            f"integral_image_{ChannelName}"[0][i+1] = image[0][i+1] + f"integral_image_{ChannelName}"[0][i]
+            integral_image[0][i+1] = ColorChannel[0][i+1] + integral_image[0][i]
         #Calculating the rest of the integral image
         for i in range(h-1):
             for j in range(w-1):
-                f"integral_image_{ChannelName}"[i+1][j+1] = image[i+1][j+1] + f"integral_image_{ChannelName}"[i][j+1] + f"integral_image_{ChannelName}"[i+1][j] - f"integral_image_{ChannelName}"[i][j]
+                integral_image[i+1][j+1] = ColorChannel[i+1][j+1] + integral_image[i][j+1] + integral_image[i+1][j] - integral_image[i][j]
 
 
     def __init__(self, image):
         #Reading height and width
         self.image = cv2.imread(image)
         self.h, self.w = self.image.shape[:2]
-        blue, green, red = cv2.split(image)
+        self.blue, self.green, self.red = cv2.split(self.image)
+        self.integral_image_blue = np.empty((self.h, self.w))
+        self.integral_image_green = np.empty((self.h, self.w))
+        self.integral_image_red = np.empty((self.h, self.w))
 
-        self.make_integral_image(self.h, self.w, blue, self.image)
+        self.make_integral_image(self.h, self.w, self.blue, self.integral_image_blue)
+        self.make_integral_image(self.h, self.w, self.green, self.integral_image_green)
+        self.make_integral_image(self.h, self.w, self.red, self.integral_image_red)
         np.savetxt('integral_image_blue.txt', self.integral_image_blue, fmt='%d')
+        np.savetxt('integral_image_green.txt', self.integral_image_green, fmt='%d')
+        np.savetxt('integral_image_red.txt', self.integral_image_red, fmt='%d')
 
-# test_image = TrainingImage('image.png')
+test_image = TrainingImage('image.png')
 # print("Height = {},  Width = {}".format(test_image.h, test_image.w))
-# cv2.imwrite('ImageTest.png', test_image.image)
+# cv2.imwrite('ImageTestgreen.png', test_image.green)
 # print(test_image.integral_image)
