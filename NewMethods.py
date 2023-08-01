51cae721b9c22e81c5b62b151c66770b0236b40b
Nehemiah Austen Pison <ezraelvio@yahoo.co.id>
2023-08-01 15:29:10 +0700

    01082023 Update

--- NewMethods.py
+++ NewMethods.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+import json
 
 class TrainingImage:
     def make_integral_image(self, h, w, ColorChannel, integral_image):
@@ -36,6 +37,34 @@ class TrainingImage:
         np.savetxt('integral_image_blue.txt', self.integral_image_blue, fmt='%d')
         np.savetxt('integral_image_green.txt', self.integral_image_green, fmt='%d')
         np.savetxt('integral_image_red.txt', self.integral_image_red, fmt='%d')
+
+class Feature:
+    def __init__(self, x, y):
+        self.featureType = "2-square-left"
+        self.integralCoordinate = [[x, y], [x + 3, y + 7], [x + 4, y], [x + 7, y + 7]]
+
+def GenerateFeatures():
+    print("Starting features generation")
+    feature_list = []
+    w = 0
+    h = 0
+    while h + 7 < 399:
+        while w + 7 < 699:
+            feature_object = Feature(h, w)
+            feature_list.append(feature_object)
+            w+=1
+        h+=1
+    while len(feature_list) > 0:
+        sacrifice_object = feature_list.pop(0)
+        with open(output_file, "Features") as json_file:
+            json.dumps(sacrifice_object.__dict__, json_file)
+
+
+        
+
+            
+            
+
 
 test_image = TrainingImage('image.png')
 # print("Height = {},  Width = {}".format(test_image.h, test_image.w))
