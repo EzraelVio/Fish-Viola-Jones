b7cbc9c00bb119755170707f2448c9ebd3bc3810
Nehemiah Austen Pison <58833986+EzraelVio@users.noreply.github.com>
2023-10-04 09:58:03 +0700

    04102023 Morn upd

--- Dataset.py
+++ Dataset.py
@@ -7,8 +7,8 @@ class Dataset:
     class_Window_offset_1 = [
         # order according to label's order in LoadImages
         # for searching mouth feature
-        (0, 0), 
         (0, 0),
+        (61, 185), 
         (0, 0),
         (0, 0)
     ]
@@ -16,8 +16,8 @@ class Dataset:
     class_Window_offset_2 = [
         # order according to label's order in LoadImages
         # for searching fin feature
-        (0, 0), 
         (0, 0),
+        (206, 50), 
         (0, 0),
         (0, 0)
     ]
@@ -25,8 +25,8 @@ class Dataset:
     class_Window_offset_3 = [
         # order according to label's order in LoadImages
         # for searching tail feature
-        (0, 0), 
         (0, 0),
+        (562, 195), 
         (0, 0),
         (0, 0)
     ]
@@ -34,19 +34,22 @@ class Dataset:
     def __init__(self, image, label, feature_list):
         self.image = image
         self.label = label
-        self.blue_Channel, self.green_Channel, self.red_Channel = combine_integral_grb(image)
-        self.window_1_features = self.Find_Feature_Value(image, feature_list, self.class_Window_offset_1[label, 0], self.class_Window_offset_1[label, 1])
+        # self.blue_Channel, self.green_Channel, self.red_Channel = combine_integral_grb(image)
+        self.window_1_features = self.Find_Feature_Value(image, feature_list, self.class_Window_offset_1[label][0], self.class_Window_offset_1[label][1])
+        # self.window_2_features = self.Find_Feature_Value(image, feature_list, self.class_Window_offset_2[label][0], self.class_Window_offset_2[label][1])
+        # self.window_3_features = self.Find_Feature_Value(image, feature_list, self.class_Window_offset_3[label][0], self.class_Window_offset_3[label][1])
 
-    def Find_Feature_Value(image, feature_list, x_offset, y_offset):
+    def Find_Feature_Value(self, image, feature_list, x_offset, y_offset):
         features = []
-        for i in range(0, feature_list):
+        for i in range(len(feature_list)):
             feature_type, x, y, width, height = feature_list[i]
             x += x_offset
             y += y_offset
             updated_feature = (feature_type, x, y, width, height)
             blue_channel_feature_value = compute_feature_with_matrix(image, 0, updated_feature)
             green_channel_feature_value = compute_feature_with_matrix(image, 1, updated_feature)
-            red_channel_feature_value = compute_feature_with_matrix(image, 3, updated_feature)
+            red_channel_feature_value = compute_feature_with_matrix(image, 2, updated_feature)
             data_features = (blue_channel_feature_value, green_channel_feature_value, red_channel_feature_value)
             features.append(data_features)
+        return features
         
--- __init__.py
+++ __init__.py
@@ -2,26 +2,28 @@ import numpy as np
 from LoadImages import *
 from HaarFeatures import *
 from IntegralImage import *
+from Dataset import *
 
 images, labels = combine_dataset()
 
 # for testing only
 features = generate_features(50, 50)
+print(len(features))
 
 # testing matrix calculation
 # features = (235, 576, 50, 50)
-feature_value_matrice = compute_feature_with_matrix(images[0], 0, "Two Horizontal", features[94702])
+# feature_value_matrice = compute_feature_with_matrix(images[0], 0, "Two Horizontal", features[94702])
 
 # testing integral image calculation
 # b, g, r = combine_integral_grb(images[0])
 # feature_value_integral = compute_feature_value(b, "Two Horizontal", features[94702])
 
-# toprint = images[0]
-# np.savetxt('Feature_visualisation.txt', toprint[235:235+50, 576:576+50, 0], fmt='%d')
+# print("starting...")
+# new_Data = Dataset(images[0], labels[0], features)
+# print(new_Data.window_1_features[0])
+
+toprint = images[0]
+np.savetxt('Feature_visualisation.txt', toprint[185:185+50, 61:61+50, 0], fmt='%d')
 # print(feature_value_matrice)
 # print(feature_value_integral)
-# cv2.imwrite('ImageTest.png', images[0])
-
-# 2147483647
-# 4294897892
-# 4294897940
+cv2.imwrite('ImageTest.png', images[0])
